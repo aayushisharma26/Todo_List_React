5f2fb14403309064d632c7fb2f8bba1fb1cdd966
Aayushi Sharma <aayushisharma20@navgurukul.org>
2023-07-25 10:16:14 +0530

    Add data

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,35 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+const people = [
+    {
+      id: 0,
+      name: 'Dear Zindagi',
+      profession: 'Drama Romance',
+      Description: "It's a good movie",
+      image: "image_1.svg",
+      votes:0
+    },
+    {
+      id: 1,
+      name: 'Mulan',
+      profession: 'Adventurous Comedy',
+      image: "Brave_img.svg",
+      votes:0
+    },
+    {
+      id: 2,
+      name: 'Chan',
+      profession: 'Adventurous Comedy',
+      image: "moana.svg",
+      votes:0
+    },
+    {
+      id: 3,
+      name: 'Mul',
+      profession: 'Adventurous Comedy',
+      image: "mulan.svg",
+      votes:0
+    }
+  ];
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,31 @@
-import React from "react"
+import React,{useState} from "react"
 
 function TodoList(){
+      const [activity,setActivity] = useState("");
+      const [listData,setlistData] = useState([]);
+      function addActivity(){
+            setlistData((listData)=>{
+                const updateList = [...listData,activity]
+                setActivity("");
+                return updateList
+            })
+      }
     return(
         <>
             <div className="container">
                 <div className="header">TODO LIST</div>
-                <input type="text" placeholder="Add Activity"/>
-                <button>Add</button>
+                <input type="text" placeholder="Add Activity" value={activity} onChange={(e)=>setActivity(e.target.value)}/>
+                <button onClick={addActivity}>Add</button>
+                <p className="List-heading">Here is your list : {")"}</p>
+                {listData!=[] && listData.map((data,i)=>{
+    return(
+        <>
+            <p key={i}>
+                <div className="listData">{data}</div>
+            </p>
+        </>
+    )
+})}
             </div>
         </>
         
